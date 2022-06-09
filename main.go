325559b7ec83bb73f9f8ec0ab7d672a17e47b052
Periklis Tsirakidis <periklis@redhat.com>
2022-06-09 19:10:21 +0200

    Clone defaultTransport

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	stdlog "log"
-	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -283,21 +282,9 @@ func main() {
 				pool := x509.NewCertPool()
 				pool.AppendCertsFromPEM(caCert)
 
-				base = &http.Transport{
-					Proxy: http.ProxyFromEnvironment,
-					DialContext: (&net.Dialer{
-						Timeout:   30 * time.Second,
-						KeepAlive: 30 * time.Second,
-					}).DialContext,
-					ForceAttemptHTTP2:     true,
-					MaxIdleConns:          100,
-					IdleConnTimeout:       90 * time.Second,
-					TLSHandshakeTimeout:   10 * time.Second,
-					ExpectContinueTimeout: 1 * time.Second,
-					TLSClientConfig: &tls.Config{
-						RootCAs: pool,
-					},
-				}
+				t := http.DefaultTransport.(*http.Transport).Clone()
+				t.TLSClientConfig = &tls.Config{RootCAs: pool}
+				base = t
 			}
 
 			p.Transport = &oauth2.Transport{
